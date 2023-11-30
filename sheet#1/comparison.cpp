1657f06d09117f5e9b017d590ef8b582048820b1
Ahmed Abdelgawad <ahmed.m.elshennawi@gmail.com>
2023-11-30 12:23:50 +0200

    problem v done

---
+++ sheet#1/comparison.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	double a , b;
+	char op;
+	cin >> a >> op >> b;
+	if(op == '>') {
+		if(a > b) {
+			cout << "Right";
+		} else {
+			cout << "Wrong";
+		}
+	} else if(op == '<') {
+		if(a < b) {
+			cout << "Right";
+		} else {
+			cout << "Wrong";
+		}
+	} else {
+		if(a == b) {
+			cout << "Right";
+		} else {
+			cout << "Wrong";
+		}
+	}
+	return 0;
+}
