0573bb525d499f97d102cf554e793b2fa14df94f
Ahmed Abdelgawad <ahmed.m.elshennawi@gmail.com>
2023-12-03 14:04:24 +0200

    problem d done

---
+++ sheet#2/fixed-password.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	while (x != 1999)
+	{
+		cout << "Wrong" << endl;
+		cin >> x;
+	}
+
+	cout << "Correct";
+	return 0;
+}
