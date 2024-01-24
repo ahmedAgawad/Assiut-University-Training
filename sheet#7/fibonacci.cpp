0b3b103d261425b1893c2bc2c2f9b1fe3e7d1f82
Ahmed Abdelgawad (ahmedAgawad) <ahmed.m.elshennawi@gmail.com>
2024-01-24 22:02:25 +0200

    problem o done

---
+++ sheet#7/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+ 
+int fib(int n) {
+	if(n <= 2) {
+		return (n-1);
+	}
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << fib(n);
+
+	return 0;
+}
